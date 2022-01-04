f5f7d338a1f809d1b1c47e573b41b51f6f388a5f
Alex Nemes <alexandru.nemes@ppb.com>
2022-01-04 20:48:15 +0200

    Add project files.

---
+++ Product_Exercise/Product.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Exercise
+{
+    enum Categories
+    {
+        Dairy, Fruit, Beverages, Bread
+
+    }
+
+    class Product
+    {
+
+        private decimal weight;
+        private string name;
+        private DateTime productionDate;
+        private DateTime expirationDate;
+        private string producer;
+        private Categories category;
+
+        public decimal Weight { get => weight; }
+        public string Name { get => name; }
+        public DateTime ProductionDate { get => productionDate; }
+        public string Producer { get => producer; }
+        public Categories Category { get => category; }
+
+
+        public DateTime ExpirationDate {
+
+            get
+            {
+                switch (category)
+                {
+                    case Categories.Dairy:
+                        return productionDate.AddDays(7);
+
+                        break;
+                    case Categories.Fruit:
+
+                        return productionDate.AddDays(2);
+                        break;
+                    case Categories.Beverages:
+
+                        return productionDate.AddYears(3);
+                        break;
+                    case Categories.Bread:
+
+                        return productionDate.AddDays(4);
+                        break;
+                    default:
+                        return DateTime.Now;
+                        break;
+                }
+            } 
+        
+        }
+
+        public Product(string name, decimal weight, DateTime productionDate, string producer, Categories category)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.productionDate = productionDate;
+            this.producer = producer;
+            this.category = category;
+
+        }
+
+    }
+}
---
+++ Product_Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Product_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       /*     Console.WriteLine("write category: ");
+            var cat = Console.ReadLine(); */
+            
+
+
+            Product product = new Product("Banane", 0.032412M, new DateTime(2020, 12, 20), "Banosh", Categories.Fruit);
+
+
+            Console.WriteLine("Category: " + product.Category + " expiration date = " + product.ExpirationDate.ToString("dd-MM-yyyy"));
+
+            Product product2 = new Product("Beer", 0.09999M, new DateTime(2020, 12, 20), "Craft", Categories.Beverages);
+
+            Console.WriteLine("Category: " + product2.Category + " expiration date = " + product2.ExpirationDate.ToString("dd-MM-yyyy"));
+
+            Product product3 = new Product("Bread", 0.4522M, new DateTime(2020, 12, 20), "Eldi", Categories.Bread);
+
+            Console.WriteLine("Category: " + product3.Category + " expiration date = " + product3.ExpirationDate.ToString("dd-MM-yyyy"));
+
+            Product product4 = new Product("Diary", 0.823424M, new DateTime(2020, 12, 20), "Milk", Categories.Dairy);
+
+            Console.WriteLine("Category: " + product4.Category + " expiration date = " + product4.ExpirationDate.ToString("dd-MM-yyyy"));
+
+        }
+    }
+}
